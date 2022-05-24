d91ab8f1de18a891de93d48fdb7f19cea16c2304
Rolake Olonisakin <rollyloni@gmail.com>
2022-05-23 18:50:20 -0700

    create route for orders to get added to their specific customer using the phonbe number

--- routes/customers.js
+++ routes/customers.js
@@ -8,8 +8,8 @@ router.use((req, res, next) => {
   next();
 });
 
-//Functions
-const readOrdersFile = () => {
+//Functions customers
+const readCustomersFile = () => {
   const customersContent = fs.readFileSync("./data/customers.json");
   return JSON.parse(customersContent);
 };
@@ -21,9 +21,38 @@ const writeCustomersFile = (data) => {
 //Routes
 
 router.get("/", (req, res) => {
-  const response = readOrdersFile();
+  const response = readCustomersFile();
 
   res.status(200).json(response);
+});
+
+router.get("/:id", (req, res) => {
+  const response = readCustomersFile();
+
+  const foundCustomer = response.find((customer) => {
+    return customer.id === req.params.id;
+  });
+
+  res.status(200).json(foundCustomer);
+});
+
+router.post("/", (req, res) => {
+  const newCustomer = {
+    id: uniqid(),
+    name: req.body.name,
+    address: req.body.address,
+    location: req.body.location,
+    country: req.body.country,
+    phone: req.body.phone,
+    email: req.body.email,
+    clientSince: req.body.clientSince,
+  };
+  const customersContent = readCustomersFile();
+  customersContent.push(newCustomer);
+
+  writeCustomersFile(customersContent);
+
+  res.status(201).json(newCustomer);
 });
 
 module.exports = router;
--- routes/orders.js
+++ routes/orders.js
@@ -18,12 +18,49 @@ const writeOrdersFile = (data) => {
   fs.writeFileSync("./data/orders.json", stringifiedData);
 };
 
+//Functions customers
+const readCustomersFile = () => {
+  const customersContent = fs.readFileSync("./data/customers.json");
+  return JSON.parse(customersContent);
+};
+const writeCustomersFile = (data) => {
+  const stringifiedData = JSON.stringify(data);
+  fs.writeFileSync("./data/customers.json", stringifiedData);
+};
+
 //Routes
 
 router.get("/", (req, res) => {
   const response = readOrdersFile();
 
   res.status(200).json(response);
+});
+
+router.post("/", (req, res) => {
+  const newOrder = {
+    id: uniqid(),
+    no: req.body.no,
+    style: req.body.style,
+    amount: req.body.amount,
+    status: req.body.status,
+    delivery: req.body.delivery,
+    payment: req.body.payment,
+    client: req.body.client,
+    phone: req.body.phone,
+  };
+  const ordersContent = readOrdersFile();
+  ordersContent.push(newOrder);
+
+  writeOrdersFile(ordersContent);
+
+  const customer = readCustomersFile();
+  const foundCustomer = customer.find((client) => {
+    return client.phone === req.body.phone;
+  });
+  foundCustomer.orders.push(newOrder);
+
+  writeCustomersFile(customer);
+  res.status(201).json(newOrder);
 });
 
 module.exports = router;
